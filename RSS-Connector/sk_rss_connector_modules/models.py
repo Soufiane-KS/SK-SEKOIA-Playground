b7f90c7290dfe28ccc322a2d66410efc6b30fd7a
Soufiane KOUSTA <soufianekousta1@gmail.com>
2026-04-07 10:48:31 +0100

    Add intake_key and intake_server to connector config

--- RSS-Connector/sk_rss_connector_modules/models.py
+++ RSS-Connector/sk_rss_connector_modules/models.py
@@ -8,3 +8,5 @@ class SkRssConnectorModuleConfiguration(BaseModel):
 class SkRssConnectorConfiguration(BaseModel):
     feed_url: str = Field(..., description="URL of the RSS feed to poll")
     frequency: int = Field(300, description="Polling frequency in seconds")
+    intake_key: str = Field(..., description="Intake key to use when sending events")
+    intake_server: str = Field("https://intake.sekoia.io", description="Intake server URL")
