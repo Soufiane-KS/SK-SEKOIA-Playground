154b229b2ac7c08b40f1b8ac2b76c531e2732dfc
Soufiane KOUSTA <soufianekousta1@gmail.com>
2026-04-07 15:07:18 +0100

    Add checkpointing to avoid duplicate events

--- RSS-Connector/CHANGELOG.md
+++ RSS-Connector/CHANGELOG.md
@@ -22,3 +22,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Changed
 
 - added a category and logo
+
+## 0.4.0 - 2026-04-07
+
+### Changed
+
+- Implemented Checkpointing to avoid duplicate feeds showing up as events
--- RSS-Connector/sk_rss_connector_modules/rss_connector.py
+++ RSS-Connector/sk_rss_connector_modules/rss_connector.py
@@ -14,15 +14,29 @@ class RssConnector(Connector):
     def _fetch_and_push(self) -> None:
         feed = feedparser.parse(self.configuration.feed_url)
         events = []
+        last_checkpoint = self.get_last_checkpoint()
+        latest_date = last_checkpoint
+
+
         for entry in feed.entries:
-            event = json.dumps({
-                "title": entry.title,
-                "link": entry.link,
-                "published": entry.get("published", ""),
-                "description": entry.get("summary", "")
-            })
+            published = entry.get("published_parsed")
+            if not published:
+                continue
+            else:
+                if last_checkpoint and published <= last_checkpoint:
+                    continue
+                else:
+                    if latest_date is None or published > latest_date:
+                        latest_date = published
+                    event = json.dumps({
+                        "title": entry.title,
+                        "link": entry.link,
+                        "published": entry.get("published", ""),
+                        "description": entry.get("summary", "")
+                    })
             events.append(event)
 
         if events:
             self.log(f"Pushing {len(events)} events", level="info")
             self.push_events_to_intakes(events=events)
+            self.set_last_checkpoint(latest_date)
